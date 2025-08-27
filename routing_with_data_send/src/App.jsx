5694376c713048f689cff0c3d1fd14dea35acbf6
Alex V <avostr@mail.ru>
2025-08-27 10:56:40 +0300

    Data trasferred from previous project. Data.js not availiable subject to replace

--- routing_with_data_send/src/App.jsx
+++ routing_with_data_send/src/App.jsx
@@ -1,28 +1,28 @@
-import './index.css'
-import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
-import Layout from "./components/Layout.jsx";
+import "./index.css"
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Cart from "./pages/Cart.jsx";
+import Category from "./pages/Category.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Layout from "./components/Layout.jsx";
+import Thanks from "./pages/Thanks.jsx";
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout /> ,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-                path: '*',
-                element: <h1
-                    className="text-red-500 text-center text-2xl font-bold mt-15">404: Page Not Found
-                    <br/>
-                    <Link to="/"><span className="underline">Home</span> Page</Link>
-                </h1>
-            }
-        ] },
-])
+/*Setting routes*/
+const router = createBrowserRouter([{
+    path: "/", element: <Layout />, children: [
+        {index: true, element: <Home />},
+        {path: "cart", element: <Cart />},
+        {path: "about", element: <About />},
+        {path: "category/:categoryId", element: <Category />},
+        {path: "product/:productId", element: <ProductDetails />},
+        {path: "thanks", element: <Thanks />},
+        {path: "*", element: <NotFound />},
+    ]
+}])
 
 export default function App() {
-    return <RouterProvider router={router}/>
+    return <RouterProvider router={router} />
 }
+
--- routing_with_data_send/src/components/Layout.jsx
+++ routing_with_data_send/src/components/Layout.jsx
@@ -1,9 +1,19 @@
-import {Outlet} from "react-router-dom";
+import {Header} from "./Header.jsx";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer.jsx";
+import {useState} from "react";
 
 export default function Layout() {
+    const [cart, setCart] = useState([]);
     return (
-        <main>
-            <Outlet />
-        </main>
+        <>
+            <Header>
+                <span> {cart.length}</span>
+            </Header>
+            <main className="max-w-2xl m-auto">
+                <Outlet cart={cart} setCart={setCart} />
+            </main>
+            <Footer/>
+        </>
     )
 }
